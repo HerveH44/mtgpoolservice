60bf36891d6b8abd4135c4f19ce84bedf7a44666
HerveH44 <herve.huneau@gmail.com>
2020-07-31 22:27:39 +0200

    avoid duplicates

--- models/entities/BoosterConfiguration.go
+++ models/entities/BoosterConfiguration.go
@@ -15,13 +15,20 @@ type Sheet struct {
 }
 
 func (s *Sheet) GetRandomCards(cardsNumber int) (ret []ProtoCard) {
-	choices := make([]utils.Choice, 0)
-	for _, conf := range s.Cards {
-		choices = append(choices, utils.NewChoice(conf, uint(conf.Weight)))
-	}
-
-	chooser := utils.NewChooser(choices...)
 	for i := 0; i < cardsNumber; i++ {
+
+		choices := make([]utils.Choice, 0)
+	OUTER:
+		for _, conf := range s.Cards {
+			for _, c := range ret {
+				if c.UUID == conf.UUID {
+					continue OUTER
+				}
+			}
+			choices = append(choices, utils.NewChoice(conf, uint(conf.Weight)))
+		}
+
+		chooser := utils.NewChooser(choices...)
 		pick := chooser.Pick().(SheetCard)
 		ret = append(ret, ProtoCard{
 			UUID: pick.UUID,
