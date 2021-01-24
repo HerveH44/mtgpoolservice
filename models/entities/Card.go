9b28308a183d4ae713fa4164ea8eea6fca357d3d
Hervé Huneau <herve.huneau@providers.glovoapp.com>
2021-01-24 23:04:10 +0100

    reorder props of card to match dr4ft original

--- models/entities/Card.go
+++ models/entities/Card.go
@@ -2,22 +2,27 @@ package entities
 
 type Card struct {
 	UUID              string `json:"uuid" gorm:"primary_key"`
-	SetID             string `json:"setCode" gorm:"index:pack_idx"`
 	Name              string `json:"name"`
-	Number            string `json:"number"`
-	Layout            string `json:"layout"`
-	Loyalty           string `json:"loyalty"`
-	Power             string `json:"power"`
-	Toughness         string `json:"toughness"`
+	FaceName          string `json:"-" gorm:"index:cubable_idx"`
+	Color             string `json:"color"`
+	SetID             string `json:"setCode" gorm:"index:pack_idx"`
 	ConvertedManaCost int    `json:"cmc"`
+	Number            string `json:"number"`
 	Type              string `json:"type"`
 	ManaCost          string `json:"manaCost"`
 	Rarity            string `json:"rarity" gorm:"index:pack_idx"`
-	Side              string `json:"side"`
-	IsAlternative     bool   `json:"isAlternative"`
-	Color             string `json:"color"`
-	ScryfallID        string `json:"scryfallId"`
 	URL               string `json:"url"`
-	Cubable           bool   `json:"-" gorm:"index:cubable_idx"`
-	FaceName          string `json:"-" gorm:"index:cubable_idx"`
+	ScryfallID        string `json:"scryfallId"` // Must be rename as identifiers with inner scryfallId
+	Layout            string `json:"layout"`
+	// isDoubleFaced
+	// flippedCardURL
+	// flippedIsBack
+	// flippedNumber
+	// text
+	Loyalty       string `json:"loyalty"`
+	Power         string `json:"power"`
+	Toughness     string `json:"toughness"`
+	Side          string `json:"side"`          // no use ?
+	IsAlternative bool   `json:"isAlternative"` // no use ?
+	Cubable       bool   `json:"-" gorm:"index:cubable_idx"`
 }
